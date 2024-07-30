07a3c18c462c28ce631413327166373dd319ab47
MaximLeontev <maximricardo8@gmail.com>
2024-07-30 20:22:42 +0200

    Removed the include directories They were getting annoying.

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ nasm Boot/Boot.asm -f bin -o Binaries/Boot.bin
 
 nasm Kernel/KernelEntry.asm -f elf -o Binaries/KernelEntry.o
 nasm Kernel/src/Interrupts.asm -f elf -o Binaries/Interrupts.o
-i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/Utils/src/LowLevelIO.c -o Binaries/LowLevelIO.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/Utils/src/MemFuncs.c -o Binaries/MemFuncs.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/src/InterruptHandlers.c -o Binaries/InterruptHandlers.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/Utils/src/Print.c -o Binaries/Print.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/src/IDT.c -o Binaries/IDT.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/src/PIC.c -o Binaries/PIC.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/Chars.c -o Binaries/Chars.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/Kernel.c -o Binaries/Kernel.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/Donut.c -o Binaries/Donut.o -IKernel/Utils/include -IKernel/include/
-i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/MemoryMap.c -o Binaries/MemoryMap.o -IKernel/Utils/include -IKernel/include/
+i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/Utils/src/LowLevelIO.c -o Binaries/LowLevelIO.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/Utils/src/MemFuncs.c -o Binaries/MemFuncs.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/src/InterruptHandlers.c -o Binaries/InterruptHandlers.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/Utils/src/Print.c -o Binaries/Print.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/src/IDT.c -o Binaries/IDT.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -mgeneral-regs-only -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -c Kernel/src/PIC.c -o Binaries/PIC.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/Chars.c -o Binaries/Chars.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/Kernel.c -o Binaries/Kernel.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/Donut.c -o Binaries/Donut.o -IKernel/Utils/src -IKernel/src
+i386-elf-gcc -std=c99 -Wall -Wextra -Wpedantic -pedantic -ffreestanding -m32 -O2 -march=pentium3 -mfpmath=sse -ftree-vectorize -c Kernel/src/MemoryMap.c -o Binaries/MemoryMap.o -IKernel/Utils/src -IKernel/src
 
 nasm Boot/Zeroes.asm -f bin -o Binaries/Zeroes.bin
 
